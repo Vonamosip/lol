925b809978fc331cdf9977067b8e6a2bb848fa27
Vonamosip <sashadrozdov11@mail.ru>
2024-07-07 23:44:09 +0300

    init: add basic page layout

--- angular.intro/Youtube-client/src/app/app.component.ts
+++ angular.intro/Youtube-client/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilterComponent } from "./header/filter/filter.component";
+import { SearchResultsComponent } from "./search/search-results/search-results.component";
+import { HeaderComponent } from "./header/header.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, FilterComponent, SearchResultsComponent, HeaderComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  title = 'Youtube-client';
+  title = 'first_task';
 }
